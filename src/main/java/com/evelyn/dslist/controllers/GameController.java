5147333b3b2d54a520ba9276f97f7b832472c2d0
evelyn-mendes <evelyn.lavinia1712@gmail.com>
2024-11-13 15:36:06 -0300

    GET game by id

--- src/main/java/com/evelyn/dslist/controllers/GameController.java
+++ src/main/java/com/evelyn/dslist/controllers/GameController.java
@@ -1,9 +1,12 @@
 package com.evelyn.dslist.controllers;
 
+import com.evelyn.dslist.dto.GameDTO;
 import com.evelyn.dslist.dto.GameMinDTO;
+import com.evelyn.dslist.entities.Game;
 import com.evelyn.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +18,11 @@ public class GameController {
 
     @Autowired
     private GameService gameService;
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        return gameService.findById(id);
+    }
 
     @GetMapping
     public List<GameMinDTO> findAll() {
